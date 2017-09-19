d6e79c275ef531f29b19107fa04ff2760b262d9a
Francisco J. Molina <franciscomoma@gmail.com>
2017-09-19 18:30:36 +0200

    Elimino import de detalle de artículo porque todavía no existe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,12 @@ import { PublicModule } from "./public-module/public.module";
 import { PrivateModule } from "./private-module/private.module";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
-import { ArticleDetailComponent } from './article-detail/article-detail.component';
 import { AppRoutingModule } from './app-routing/app-routing.module';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ArticleDetailComponent
+    AppComponent
   ],
   imports: [
     AppRoutingModule,
